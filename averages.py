1670bb49489b501feb53889fb47ab27058ef039a
Andrew Thomas <andrewthomas@andrews-mbp.ssw.umb.edu>
2018-10-11 17:47:04 -0400

    Moved typecasting to input

--- averages.py
+++ averages.py
@@ -1,31 +1,32 @@
 def mean(ar, n):
-    n = int(n)
     total = 0
     for num in ar:
-        num = int(num)
+        num = num
         total += num   
     return total / n
 
 def median(ar, n):
-    n = int(n)
     if n % 2 == 0:
-        return int(ar[n//2]) + int(ar[(n//2) + 1]) / 2
+        return ar[n//2] + ar[(n//2) + 1] / 2
     else:
-        return int(ar[n//2])
+        return ar[n//2]
 
 def mode(ar, n):
     nums = {}
     for num in ar:
         if num in nums:
-            nums[int(num)] += 1
+            nums[num] += 1
         else:
-            nums[int(num)] = 1
+            nums[num] = 1
     return max(nums.values())
 
 if __name__ == '__main__':
-    n = input()
+    n = int(input())
+    arin = []
+    arin = input().split()
     ar = []
-    ar = input().split()
+    for num in arin:
+        ar.append(int(num))
 
     meanout = mean(ar,n)
     print(str(meanout))
