de5ed2c0d7a6da27e9374b6701a90ebf10ad8741
Andrew Thomas <andrewthomas@andrews-mbp.ssw.umb.edu>
2018-10-11 18:39:55 -0400

    that was easy

---
+++ weighted_average.py
@@ -0,0 +1,15 @@
+if __name__ == '__main__':
+    n = int(input())
+    xin = input().split()
+    win = input().split()
+
+    x = []
+    w = []
+    for i in xin:
+        x.append(int(i))
+    for i in win:
+        w.append(int(i))
+    total = 0
+    for i in range(0, n):
+        total += x[i] * w[i]
+    print (str(total/sum(w)))
